ce5d1206f183afd6300ebfbcf4e9df8f99cb4ff7
Rui Lebre <ruilebre@ua.pt>
2021-02-28 17:34:43 +0000

    Added prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,38 @@
 import * as React from 'react';
-import {DicoogleAccess} from 'dicoogle-client';
-import {Webcore, SlotHTMLElement, PluginData} from './webcore';
+import { DicoogleAccess } from 'dicoogle-client';
+import { Webcore, SlotHTMLElement, PluginData } from './webcore';
 
 // global Dicoogle access instance
 declare const Dicoogle: DicoogleAccess & Webcore;
 
 interface Props {
-    // TODO define props here
+  // TODO define props here
 }
 
 interface State {
-    // TODO define state shere
+  // TODO define state shere
 }
 
 class PluginComponent extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (<div>Hello, Dicoogle!</div>);
-    }
+  render() {
+    return <div>Hello, Dicoogle!</div>;
+  }
 }
 
 export default class MyPlugin {
-    
-    constructor() {
-        // TODO initialize plugin here
-    }
-    
-    render(parent: HTMLElement, slot: SlotHTMLElement): typeof PluginComponent {
-        return PluginComponent;
-    }
+  constructor() {
+    // TODO initialize plugin here
+  }
 
-    onReceiveData(data: PluginData) {
-        // retrieve data here
-    }
+  render(parent: HTMLElement, slot: SlotHTMLElement): typeof PluginComponent {
+    return PluginComponent;
+  }
+
+  onReceiveData(data: PluginData) {
+    // retrieve data here
+  }
 }
